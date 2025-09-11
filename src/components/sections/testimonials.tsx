8d40707c104b5baee3426122d430c56dde8ba24a
Porto Vale <portovale.ad@gmail.com>
2025-09-11 20:42:54 +0000

    retire toda deiv e jogue o carrossel para a deiv de cima transformando a

--- src/components/sections/testimonials.tsx
+++ src/components/sections/testimonials.tsx
@@ -38,20 +38,12 @@ export default function Testimonials() {
   return (
     <section id="testimonials" className="w-full py-12 md:py-24 lg:py-32">
       <div className="container mx-auto px-4 md:px-6">
-        <div className="flex flex-col items-center justify-center space-y-4 text-center">
-          <div className="space-y-2">
-            
-            <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-              
-            </p>
-          </div>
-        </div>
         <Carousel
           opts={{
             align: 'start',
             loop: true,
           }}
-          className="mx-auto mt-12 w-full max-w-4xl"
+          className="mx-auto w-full max-w-4xl"
         >
           <CarouselContent>
             {testimonials.map((testimonial, index) => {
